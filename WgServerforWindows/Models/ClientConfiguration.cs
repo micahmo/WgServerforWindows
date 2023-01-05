56d14c7892717cfdb04fabd53995f974cbadb6a7
Micah Morrison <micahmorrison95@gmail.com>
2023-01-05 11:02:32 -0500

    Put back newline without extra whitespace

--- WgServerforWindows/Models/ClientConfiguration.cs
+++ WgServerforWindows/Models/ClientConfiguration.cs
@@ -66,6 +66,7 @@ namespace WgServerforWindows.Models
                         })
                         .Where(a => a != null) ?? Enumerable.Empty<IPAddress>())
                         .ToList(); // Prevent multiple enumeration
+
                     var newClientAddresses = new HashSet<string>();
                     var serverAddressesToConsider = new List<IPNetwork>(serverAddresses); // Copy
 
